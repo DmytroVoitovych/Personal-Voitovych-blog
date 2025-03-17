bbeac3104708b6121712282eeb8ec6ddac34816d
DmytroVoitovych <begunec23@gmail.com>
2025-03-17 21:06:53 +0200

    header is ready

--- src/components/navComponent/NavComponent.tsx
+++ src/components/navComponent/NavComponent.tsx
@@ -1,16 +1,28 @@
+"use client";
 import Link from "next/link";
-import styles from './NavComponent.module.css';
+import styles from "./NavComponent.module.css";
+import { usePathname } from "next/navigation";
 
-
-const NavComponent = ({mobile}:{mobile:boolean}) => {
- 
+const NavComponent = ({ mobile }: { mobile: boolean }) => {
+  const pathname = usePathname();
+  
+  const isActive = (linkName: string): string | "activeLink" =>
+    pathname === linkName ? styles?.activeLink || "" : "";
 
   return (
-   <nav className={`${styles.navBlock} text-preset-7`}  data-mobile={mobile} >
-      <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/about">About</Link>
-      <Link href="/newsletter">Newsletter</Link>
+    <nav className={`${styles.navBlock} text-preset-7`} data-mobile={mobile}>
+      <Link className={isActive("/")} href="/">
+        Home
+      </Link>
+      <Link className={isActive("/blog")} href="/blog">
+        Blog
+      </Link>
+      <Link className={isActive("/about")} href="/about">
+        About
+      </Link>
+      <Link className={isActive("/newsletter")} href="/newsletter">
+        Newsletter
+      </Link>
     </nav>
   );
 };
